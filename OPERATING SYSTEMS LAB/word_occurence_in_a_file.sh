1d71f4f36acd6fa00e890b2e8f9ebbc72d71a8fa
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2022-01-05 12:49:00 +0530

    Update word_occurence_in_a_file.sh

--- OPERATING SYSTEMS LAB/word_occurence_in_a_file.sh
+++ OPERATING SYSTEMS LAB/word_occurence_in_a_file.sh
@@ -12,7 +12,7 @@ egrep -o "\b[[:alpha:]]+\b" $filename | \
   awk '{ count[$0]++ }
     END {printf("%-14s%s\n","Word","Count") ;
       for(ind in count)
-        { printf("%-14s%d\n",ind,count[ind]); 
+        { printf("%-14s%s\n",ind,count[ind]); 
         }
       }'
 
